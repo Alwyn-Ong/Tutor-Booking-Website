42c51fdf45d071395175450b2bd478cfc87dd606
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2021-03-13 12:19:00 +0800

    Added rating and placeholder for timetable

--- app/frontend/src/views/Tutor.js
+++ app/frontend/src/views/Tutor.js
@@ -1,5 +1,6 @@
 import {
   Avatar,
+  Divider,
   Grid,
   IconButton,
   makeStyles,
@@ -8,6 +9,7 @@ import {
 import React from "react";
 import { Page } from "../components/Page";
 import { Email, Facebook, LinkedIn } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,7 +37,7 @@ const Tutor = ({ data }) => {
     email: "alwyn.ong.2018@sis.smu.edu.sg",
     qualification: "A-Level",
     gender: "Male",
-    review: 4,
+    review: 4.5,
     price: 100,
   };
 
@@ -45,68 +47,81 @@ const Tutor = ({ data }) => {
 
   return (
     <Page>
-      <Grid container spacing={3}>
-        <Grid item xs={6} lg={4}>
-          <Avatar className={classes.large} />
+      <Grid container spacing={3} direction="column">
+        <Grid container item spacing={3}>
+          <Grid item xs={6} lg={4}>
+            <Avatar className={classes.large} />
+          </Grid>
+          <Grid item xs={6} lg={4}>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Typography>{data.name}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Typography>{data.bio}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid item>
+                <IconButton
+                  onClick={() => {
+                    window.open(`http://${data.facebook}`);
+                  }}
+                >
+                  <Facebook />
+                </IconButton>
+                <IconButton
+                  onClick={() => {
+                    window.open(`http://${data.linkedin}`);
+                  }}
+                >
+                  <LinkedIn />
+                </IconButton>
+                <IconButton
+                  onClick={() => {
+                    window.href(`${data.email}`);
+                  }}
+                >
+                  <Email />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Typography>{data.qualification}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Typography>{data.gender}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Typography>{`$${data.price}/hr`}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Rating
+                  name="half-rating-read"
+                  value={data.review}
+                  readOnly
+                  precision={0.5}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={6} lg={4}>
-          <Grid container spacing={3}>
-            <Grid item>
-              <Typography>{data.name}</Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item>
-              <Typography>{data.bio}</Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item>
-              <IconButton
-                onClick={() => {
-                  window.open(`http://${data.facebook}`);
-                }}
-              >
-                <Facebook />
-              </IconButton>
-              <IconButton
-                onClick={() => {
-                  window.open(`http://${data.linkedin}`);
-                }}
-              >
-                <LinkedIn />
-              </IconButton>
-              <IconButton
-                onClick={() => {
-                  window.href(`${data.email}`);
-                }}
-              >
-                <Email />
-              </IconButton>
-            </Grid>
-          </Grid>
+        <Grid item>
+          <Divider variant="middle"/>
         </Grid>
-        <Grid item xs={12} lg={4}>
-          <Grid container spacing={3}>
-            <Grid item>
-              <Typography>{data.qualification}</Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item>
-              <Typography>{data.gender}</Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item>
-              <Typography>{`$${data.price}/hr`}</Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item>
-              <Typography>{`${data.review}/5`}</Typography>
-            </Grid>
-          </Grid>
+        <Grid item>
+          Placeholder for timetable
         </Grid>
       </Grid>
     </Page>
