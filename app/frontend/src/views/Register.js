49c997316bc9b499cf095835b7b9cc6dff313050
wilsonng70 <wilson.ng.2017@sis.smu.edu.sg>
2021-03-13 21:22:55 +0800

    Changes to css and form

--- app/frontend/src/views/Register.js
+++ app/frontend/src/views/Register.js
@@ -18,24 +18,37 @@ function Register() {
   return (
     <form action="mailto:s_sample@yahoo.com" method="get" id="frm">
     <fieldset id="fld">
-        <legend>Sign Up</legend>
-        <label for="email"></label>
-        <input type="email" size="30" id="email" placeholder="Enter a Valid Email" maxlength="30" required></input><br></br><br></br>
+        <legend>Register</legend>
 
-        <label for="pass"></label>
-        <input type="password" size="30" id="pass" placeholder="Enter Password" maxlength="30" required></input><br></br><br></br>
+        <input type="email" size="30" id="email" placeholder="Enter a Valid Email" maxlength="30" required></input>
+        <br></br>
+        <br></br>
 
-        <label for="pass"></label>
-        <input type="password" size="30" id="repeatpass" placeholder="Repeat Password" maxlength="40" required></input><br></br><br></br>
+        <input type="text" size="30" id="name" placeholder="Please enter your name" maxlength="30" required></input>
+        <br></br>
+        <br></br>
 
-        Birthday: <input type="date"></input><br></br><br></br><br></br><br></br>
+        <input type="number" size="30" id="number" placeholder="What's your number" maxlength="8" required></input>
+        <br></br>
+        <br></br>
 
-        <label><input type="radio" name="st" required>I accepted the terms of use, cookie and privacy policy</input></label><br></br><br></br><br></br>
+        <input type="text" size="30" id="gender" placeholder="Your gender" maxlength="30" required></input>
+        <br></br>
+        <br></br>
+        {/* Copy radio button */}
 
-        <input type="submit" value="Create Account" class="btn"></input>
-        <input type="reset" value="Clear" class="btn"></input>
-        <br></br><br></br>
-        Do you already have an account? <a href="">Login</a><br></br><br></br><br></br>
+        Birthday: <input type="date"></input>
+        <br></br>
+        <br></br>
+
+        <input type="submit" value="Set up account" class="btn"></input>
+
+        <br></br>
+        <br></br>
+        
+        <br></br>
+        <br></br>
+        <br></br>
       </fieldset>
   </form>
 
