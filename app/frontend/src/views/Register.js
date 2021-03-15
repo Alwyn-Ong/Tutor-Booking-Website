acff2a846fcd26743662f64c6e6969424e96729a
wilsonng70 <wilson.ng.2017@sis.smu.edu.sg>
2021-03-15 16:04:15 +0800

    Update
    1. Changed to react component
    2. Can capture updated value
    3. Form validated for name, number, sex, birthday (partial)
    4. Email is readonly

--- app/frontend/src/views/Register.js
+++ app/frontend/src/views/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Page } from "../components/Page";
 import { makeStyles, Radio,RadioGroup,FormLabel, FormControlLabel } from "@material-ui/core";
 import "./css/register.css"
@@ -12,49 +12,89 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Register() {
-  const userEmail = "Tiffany@gmail.com" //This can be changed to take in the user email from Gmail
+
+class Register extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      userEmail: "Tiffany@gmail.com", //Can take in other values
+      name: "",
+      number: "",
+      birthday: "",
+      sex: ""
+    }
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleNumChange = this.handleNumChange.bind(this);
+    this.handleBirthdayChange = this.handleBirthdayChange.bind(this);
+    this.handleSexChange = this.handleSexChange.bind(this);
+  }
+
+  handleLogin(e){
+    alert('A name was submitted: ' + this.state.sex);
+    // need to send data back to DB from here
+  }
+
+  handleNameChange(e){
+    this.setState({name: e.target.value});
+  }
+
+  handleNumChange(e){
+    this.setState({number: e.target.value});
+  }
+
+  handleBirthdayChange(e){
+    this.setState({birthday: e.target.value});
+  }
+
+  handleSexChange(e){
+    this.setState({sex: e.target.value});
+  }
+    
+  render() {
+    return (
+      <form action="" method="get" id="frm" onSubmit={this.handleLogin}>
+      <fieldset id="fld">
+          <legend>Register!</legend>
+          <h2>Complete your registration by filling up the form</h2>
+          <br></br>
+          <br></br>
   
-  return (
-    <form action="" method="get" id="frm">
-    <fieldset id="fld">
-        <legend>Register!</legend>
-        <h2>Complete your registration by filling up the form</h2>
-        <br></br>
-        <br></br>
-
-        <input type="text" size="30" id="email" value={userEmail} maxlength="30" readonly></input>
-        
-        <br></br>
-        <br></br>
-
-        <input type="text" size="30" id="name" placeholder="Your Name" maxlength="30" required></input>
-        
-        <br></br>
-        <br></br>
-
-        <input type="tel" size="30" id="phone" placeholder="Your Phone Number" maxlength="8" pattern="[0-9]{8}" required></input>
-        <br></br>
-        <br></br>
-        <label id="gender" required>Please indicate your sex/gender:</label><br></br>
-        <input type="radio" name="sex" value="Male" /><span> Male</span><br></br>
-        <input type="radio" name="sex" value="Female" /><span> Female</span><br></br>
-        <input type="radio" name="sex" value="Others" /><span> Others</span>
-        
-        <br></br>
-        <br></br>
-        {/* Copy radio button */}
-        Birthday: <input type="date" required></input>
-        <br></br>
-        <br></br>
-        
-
-        <input type="submit" value="Create Account" class="btn"></input>
-
-    </fieldset>
-  </form>
-
-  )
+          <input type="text" size="30" id="email" value={this.state.userEmail} maxlength="30" readOnly></input>
+          
+          <br></br>
+          <br></br>
+  
+          <input type="text" size="30" id="name" value={this.state.name} onChange={this.handleNameChange} placeholder="Your Name" maxlength="30" required></input>
+          
+          <br></br>
+          <br></br>
+  
+          <input type="tel" size="30" id="phone" value={this.state.number} onChange={this.handleNumChange} placeholder="Your Phone Number" maxlength="8" pattern="[0-9]{8}" required></input>
+          <br></br>
+          <br></br>
+          <div >
+            <label id="sex" required>Please indicate your sex/gender:</label><br></br> 
+            <input type="radio" name="sex" value="Male" onChange={this.handleSexChange} required/><span> Male</span><br></br>
+            <input type="radio" name="sex" value="Female" onChange={this.handleSexChange} /><span> Female</span><br></br>
+            <input type="radio" name="sex" value="Others" onChange={this.handleSexChange} /><span> Others</span>
+          </div>
+          
+          
+          <br></br>
+          <br></br>
+          {/* Copy radio button */}
+          Birthday: <input type="date" value={this.state.birthday} onChange={this.handleBirthdayChange} required></input>
+          <br></br>
+          <br></br>
+          
+          <input type="submit" value="Create Account" class="btn"></input>
+  
+      </fieldset>
+    </form>
+  
+    )
+  }
 }
 
 export default Register;
