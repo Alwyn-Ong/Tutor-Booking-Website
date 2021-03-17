232f9627037613d00427d8b508e763f3e8f69a69
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2021-03-17 20:59:04 +0800

    Formatted SearchBar

--- app/frontend/src/components/HomePage/SearchBar.js
+++ app/frontend/src/components/HomePage/SearchBar.js
@@ -1,51 +1,53 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import './SearchBar.css'
-import FilterPanel from './FilterPanel';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "./SearchBar.css";
+import FilterPanel from "./FilterPanel";
+import Grid from "@material-ui/core/Grid";
 import Results from "./Results";
-import Sticky from 'react-sticky-el';
-import Container from '@material-ui/core/Container';
-import ParallaxCarousel from './ParallaxCarousel';
-import NoticeBoard from './NoticeBoard';
+import Sticky from "react-sticky-el";
+import Container from "@material-ui/core/Container";
+import ParallaxCarousel from "./ParallaxCarousel";
+import NoticeBoard from "./NoticeBoard";
 
 function SearchBar() {
-    // const classes = useStyles();
-    const useStyles = makeStyles({
-        root: {
-            position: "sticky",
-            top: "1rem",
-            //   minWidth: "275"
-        }
-    });
-    const classes = useStyles();
+  // const classes = useStyles();
+  const useStyles = makeStyles({
+    root: {
+      position: "sticky",
+      top: "1rem",
+      //   minWidth: "275"
+    },
+  });
+  const classes = useStyles();
 
-    const [filter, setFilter] = React.useState({
-        gender: '',
-        price: '',
-        level: '',
-        qualification: '',
-        selectedLocations:[]
-    });
+  const [filter, setFilter] = React.useState({
+    gender: "",
+    price: "",
+    level: "",
+    qualification: "",
+    selectedLocations: [],
+  });
 
-    
-    return (
-        <Grid>
-                <Container className="heading">
-                    <form>
-                        <input className="search-bar" placeholder="Search for tutors!"></input>
-                        <input type="submit" value="GO" href="#"></input>
-                    </form>
-                    <FilterPanel setState={setFilter} state={filter}></FilterPanel>
-                </Container>
-                <Container className="carousellContainer">
-                    <NoticeBoard></NoticeBoard>
-                </Container>
-                <Container className="tracks">
-                    <Results filterConditions={filter}></Results>
-                </Container>
-        </Grid>
-    );
+  return (
+    <Grid>
+      <Container className="heading">
+        <form>
+          <input
+            className="search-bar"
+            placeholder="Search for tutors!"
+          ></input>
+          <input type="submit" value="GO" href="#"></input>
+        </form>
+        <FilterPanel setState={setFilter} state={filter}></FilterPanel>
+      </Container>
+      <Container className="carousellContainer">
+        <NoticeBoard></NoticeBoard>
+      </Container>
+      <Container className="tracks">
+        <Results filterConditions={filter}></Results>
+      </Container>
+    </Grid>
+  );
 }
 
 export default SearchBar;
