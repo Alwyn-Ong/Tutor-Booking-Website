1d2e2e6b0b3e337e4ef271cbd62b3f6c3ba909aa
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-03-17 17:28:59 +0800

    image upload and get API

---
+++ app/backend/src/main/java/TutorBookingWebsite/model/Image.java
@@ -0,0 +1,66 @@
+package TutorBookingWebsite.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "image")
+public class Image {
+	public Image() {
+		super();
+	}
+
+	public Image(int tutorId, String type, byte[] picByte) {
+
+		this.type = type;
+		this.picByte = picByte;
+	}
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "type")
+	private String type;
+	// image bytes can have large lengths so we specify a value
+	// which is more than the default length for picByte column
+	@Column(name = "picByte")
+	private byte[] picByte;
+	private int userId;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public byte[] getPicByte() {
+		return picByte;
+	}
+
+	public void setPicByte(byte[] picByte) {
+		this.picByte = picByte;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+}
