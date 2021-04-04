8d4519d0578e736d6b891d004798bf14e824c112
Alwyn-Ong <alwyn.ong.2018@sis.smu.edu.sg>
2021-04-04 16:41:39 +0800

    Random on render

--- app/frontend/src/components/Tutor/Stats/Doughnut.js
+++ app/frontend/src/components/Tutor/Stats/Doughnut.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
-const data = {
-    labels: [
-        'Primary',
-        'Secondary',
-        'JC'
-    ],
-    datasets: [{
-        data: [
-            Math.ceil(Math.random() * 20),
-            Math.ceil(Math.random() * 20),
-            Math.ceil(Math.random() * 20),
-        ],
-        backgroundColor: [
-            '#8dace7',
-            '#71deb9',
-            '#ef869e'
-        ],
-        hoverBackgroundColor: [
-            '#7097e1',
-            '#4dd6a7',
-            '#eb6886'
-        ]
-    }]
-};
+
 
 class DoughnutExample extends React.Component {
 
     render() {
+        const data = {
+            labels: [
+                'Primary',
+                'Secondary',
+                'JC'
+            ],
+            datasets: [{
+                data: [
+                    Math.ceil(Math.random() * 20),
+                    Math.ceil(Math.random() * 20),
+                    Math.ceil(Math.random() * 20),
+                ],
+                backgroundColor: [
+                    '#8dace7',
+                    '#71deb9',
+                    '#ef869e'
+                ],
+                hoverBackgroundColor: [
+                    '#7097e1',
+                    '#4dd6a7',
+                    '#eb6886'
+                ]
+            }]
+        };
         return (
             <div>
                 <Doughnut data={data}/>
--- app/frontend/src/components/Tutor/Stats/Radar.js
+++ app/frontend/src/components/Tutor/Stats/Radar.js
@@ -1,41 +1,40 @@
 import React from "react";
 import { Radar } from "react-chartjs-2";
 
-const data = {
-  // labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
-  labels: ["Price", "Reviews", "Rating", "Subjects", "Timeslots"],
-  datasets: [
-    // {
-    //   label: "Average Tutor",
-    //   backgroundColor: "rgba(179,181,198,0.2)",
-    //   borderColor: "rgba(179,181,198,1)",
-    //   pointBackgroundColor: "rgba(179,181,198,1)",
-    //   pointBorderColor: "#fff",
-    //   pointHoverBackgroundColor: "#fff",
-    //   pointHoverBorderColor: "rgba(179,181,198,1)",
-    //   data: [50, 50, 50, 50, 50],
-    // },
-    {
-      label: "Current Tutor",
-      backgroundColor: "rgba(255,99,132,0.2)",
-      borderColor: "rgba(255,99,132,1)",
-      pointBackgroundColor: "rgba(255,99,132,1)",
-      pointBorderColor: "#fff",
-      pointHoverBackgroundColor: "#fff",
-      pointHoverBorderColor: "rgba(255,99,132,1)",
-      data: [
-        Math.ceil(Math.random() * 85 + 15),
-        Math.ceil(Math.random() * 85 + 15),
-        Math.ceil(Math.random() * 85 + 15),
-        Math.ceil(Math.random() * 85 + 15),
-        Math.ceil(Math.random() * 85 + 15),
-      ],
-    },
-  ],
-};
-
 class RadarExample extends React.Component {
   render() {
+    const data = {
+      // labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+      labels: ["Price", "Reviews", "Rating", "Subjects", "Timeslots"],
+      datasets: [
+        // {
+        //   label: "Average Tutor",
+        //   backgroundColor: "rgba(179,181,198,0.2)",
+        //   borderColor: "rgba(179,181,198,1)",
+        //   pointBackgroundColor: "rgba(179,181,198,1)",
+        //   pointBorderColor: "#fff",
+        //   pointHoverBackgroundColor: "#fff",
+        //   pointHoverBorderColor: "rgba(179,181,198,1)",
+        //   data: [50, 50, 50, 50, 50],
+        // },
+        {
+          label: "Current Tutor",
+          backgroundColor: "rgba(255,99,132,0.2)",
+          borderColor: "rgba(255,99,132,1)",
+          pointBackgroundColor: "rgba(255,99,132,1)",
+          pointBorderColor: "#fff",
+          pointHoverBackgroundColor: "#fff",
+          pointHoverBorderColor: "rgba(255,99,132,1)",
+          data: [
+            Math.ceil(Math.random() * 85 + 15),
+            Math.ceil(Math.random() * 85 + 15),
+            Math.ceil(Math.random() * 85 + 15),
+            Math.ceil(Math.random() * 85 + 15),
+            Math.ceil(Math.random() * 85 + 15),
+          ],
+        },
+      ],
+    };
     return (
       <div>
         <Radar data={data} />
